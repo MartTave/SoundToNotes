aa4b6c9e7a24c40650d0fd3a3647f04ad55bcb18
MartTave <martin.tavernier256@gmail.com>
2023-08-30 15:32:52 +0200

    ça marche pas encore :(

--- Conversion.py
+++ Conversion.py
@@ -2,13 +2,13 @@ import librosa
 import mido
 
 # Charger le fichier audio
-fichier_audio = 'MusicTest/BrotherJacques.wav'
+fichier_audio = 'MusicTest/c5.mp3'
 y, sr = librosa.load(fichier_audio)
+y = librosa.to_mono(y)
 
 # Estimer les débuts de notes et les hauteurs de notes
-onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
-pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
-
+onset_frames = librosa.onset.onset_detect(y=y, sr=sr, units="time")
+pitches, magnitudes = librosa.piptrack(y=y, sr=sr, threshold=0.5)
 # Créer des messages MIDI
 sortie_midi = mido.MidiFile()
 
@@ -19,6 +19,7 @@ sortie_midi.tracks.append(piste)
 # Définir le tempo en microsecondes par temps (500000 microsecondes par temps = 120 BPM)
 tempo = mido.bpm2tempo(120)
 piste.append(mido.MetaMessage('set_tempo', tempo=tempo))
+
 
 # Stocker les notes actives
 notes_actives = set()
@@ -37,13 +38,14 @@ for frame in onset_frames:
 
     # enlever les notes qui ne sont plus actives
     for numero_note in notes_a_eteindre:
-        note_off = mido.Message('note_off', note=numero_note, velocity=64, time=0)
-        piste.append(note_off)
+        #note_off = mido.Message('note_off', note=numero_note, velocity=64, time=0)
+        #piste.append(note_off)
         notes_actives.remove(numero_note)
 
     # ajouter les nouvelles notes et les ajouter à notes_actives
     for numero_note in nouvelles_notes:
         if numero_note not in notes_actives:
+            print(numero_note)
             note_on = mido.Message('note_on', note=numero_note, velocity=64, time=0)
             piste.append(note_on)
             notes_actives.add(numero_note)
@@ -53,8 +55,8 @@ for frame in onset_frames:
     avance_temporelle = int(sr * duree_trame_ms)
 
     for numero_note in notes_actives:
-        note_off = mido.Message('note_off', note=numero_note, velocity=64, time=avance_temporelle)
+        note_off = mido.Message('note_off', note=numero_note, velocity=64, time=0)
         piste.append(note_off)
 
 # Enregistrer le fichier MIDI
-sortie_midi.save('Brother.mid')
+sortie_midi.save('./c5.mid')
---
+++ conversion_martin.py
@@ -0,0 +1,72 @@
+import librosa
+import mido
+
+INPUT = "./MusicTest/piano_range.wav"
+OUTPUT = "./results/test.mid"
+ABS_TRESHOLD = 7
+TRESHOLD = 0.8
+
+data, sr = librosa.load(INPUT)
+data = librosa.to_mono(data)
+tempo = int(librosa.feature.tempo(y=data)[0])
+tempo = 2000
+print("Tempo is : ", tempo)
+# This indicate the number of frames to take into account to
+# analyse 1 beat
+framePerBeat = int(1 / (tempo / 60) * sr)
+
+midiOut = mido.MidiFile()
+
+track = mido.MidiTrack()
+track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(tempo)))
+
+
+
+
+nbrOfBeat = int((len(data) - (len(data) % framePerBeat)) / framePerBeat)
+
+print("Sopng got", nbrOfBeat, " beats")
+
+activeNotes = []
+
+lastEventTick = 0
+
+for b in range(0, nbrOfBeat):
+    # Current data should cover a beat in the song
+    currentData = data[b*framePerBeat:(b + 1) * framePerBeat - 1]
+    pitches, magnitudes = librosa.piptrack(y=currentData, sr=sr, n_fft=6)
+    notes = []
+    maxMag = magnitudes.max()
+    for i in range(0, len(pitches)):
+        for j in range(0, len(pitches[i])):
+            if(pitches[i][j] != 0.0):
+                if(magnitudes[i][j] < ABS_TRESHOLD and magnitudes[i][j] < maxMag * TRESHOLD):
+                    continue
+                print(librosa.hz_to_note(pitches[i][j]), pitches[i][j], magnitudes[i][j])
+                midiNote = round(librosa.hz_to_midi(pitches[i][j]))
+                if notes.count(midiNote) == 0:
+                    notes.append(midiNote)
+    # Here we have all the notes in the time frame taken
+
+    treatedNotes = []
+    for n in notes:
+        # This is here to keep track of what needs to be removed from active notes
+        treatedNotes.append(n)
+        if activeNotes.count(n) == 0:
+            # We need to activate the note !
+            activeNotes.append(n)
+            currentTick = b * midiOut.ticks_per_beat
+            tickDelta = currentTick - lastEventTick
+            track.append(mido.Message("note_on", note=n, time=tickDelta))
+            lastEventTick = currentTick
+    for n in activeNotes:
+        if treatedNotes.count(n) == 0:
+            # This means that the note was not active anymore
+            activeNotes.remove(n)
+            currentTick = b * midiOut.ticks_per_beat
+            tickDelta = currentTick - lastEventTick
+            track.append(mido.Message("note_off", note=n, time=tickDelta))
+            lastEventTick = currentTick
+
+midiOut.tracks.append(track)
+midiOut.save(OUTPUT)
