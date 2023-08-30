b51d2cba31785a6a2d9a840bf9ae4df45bc918cf
Francisco RIbeiro <74601529+franciscoribeiro99@users.noreply.github.com>
2023-08-30 08:28:58 +0200

    new code: output ok TODO: Precision

--- Conversion.py
+++ Conversion.py
@@ -1,61 +1,60 @@
 import librosa
 import mido
 
-# Load the audio file
-audio_file = 'MusicTest/FrankSinatra.wav'
-y, sr = librosa.load(audio_file)
+# Charger le fichier audio
+fichier_audio = 'MusicTest/BrotherJacques.wav'
+y, sr = librosa.load(fichier_audio)
 
-# Estimate note onsets and pitches
+# Estimer les débuts de notes et les hauteurs de notes
 onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
 pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
 
-# Create MIDI messages
-output_midi = mido.MidiFile()
+# Créer des messages MIDI
+sortie_midi = mido.MidiFile()
 
-# Add a new MIDI track
-track = mido.MidiTrack()
-output_midi.tracks.append(track)
+# Ajouter une nouvelle piste MIDI
+piste = mido.MidiTrack()
+sortie_midi.tracks.append(piste)
 
-# Set the tempo in microseconds per beat (500000 microseconds per beat = 120 BPM)
+# Définir le tempo en microsecondes par temps (500000 microsecondes par temps = 120 BPM)
 tempo = mido.bpm2tempo(120)
-track.append(mido.MetaMessage('set_tempo', tempo=tempo))
+piste.append(mido.MetaMessage('set_tempo', tempo=tempo))
 
-# Store active notes
-active_notes = set()
+# Stocker les notes actives
+notes_actives = set()
 
-# Add note events
+# Ajouter les événements de notes
 for frame in onset_frames:
-    new_notes = set()
-    for pitch_idx in range(pitches.shape[0]):
-        frequency = pitches[pitch_idx, frame]
-        if frequency > 0:
-            note_number = int(round(librosa.hz_to_midi(frequency)))
-            if 0 <= note_number <= 127:
-                new_notes.add(note_number)
+    nouvelles_notes = set()
+    for indice_hauteur in range(pitches.shape[0]):
+        frequence = pitches[indice_hauteur, frame]
+        if frequence > 0:
+            numero_note = int(round(librosa.hz_to_midi(frequence)))
+            nouvelles_notes.add(numero_note)
 
-    # Find notes that need to be turned off
-    notes_to_turn_off = active_notes - new_notes
+    # Trouver les notes à enlever
+    notes_a_eteindre = notes_actives - nouvelles_notes
 
-    # Turn off notes that are no longer active
-    for note_number in notes_to_turn_off:
-        note_off = mido.Message('note_off', note=note_number, velocity=64, time=0)
-        track.append(note_off)
-        active_notes.remove(note_number)
+    # enlever les notes qui ne sont plus actives
+    for numero_note in notes_a_eteindre:
+        note_off = mido.Message('note_off', note=numero_note, velocity=64, time=0)
+        piste.append(note_off)
+        notes_actives.remove(numero_note)
 
-    # Turn on new notes and add them to active_notes
-    for note_number in new_notes:
-        if note_number not in active_notes:
-            note_on = mido.Message('note_on', note=note_number, velocity=64, time=0)
-            track.append(note_on)
-            active_notes.add(note_number)
+    # ajouter les nouvelles notes et les ajouter à notes_actives
+    for numero_note in nouvelles_notes:
+        if numero_note not in notes_actives:
+            note_on = mido.Message('note_on', note=numero_note, velocity=64, time=0)
+            piste.append(note_on)
+            notes_actives.add(numero_note)
 
-    # Calculate the time_advance
-    frame_duration_ms = 0.01
-    time_advance = int(sr * frame_duration_ms)
+    # Calculer l'avance_temporelle
+    duree_trame_ms = 0.01
+    avance_temporelle = int(sr * duree_trame_ms)
 
-    for note_number in active_notes:
-        note_off = mido.Message('note_off', note=note_number, velocity=64, time=time_advance)
-        track.append(note_off)
+    for numero_note in notes_actives:
+        note_off = mido.Message('note_off', note=numero_note, velocity=64, time=avance_temporelle)
+        piste.append(note_off)
 
-# Save the MIDI file
-output_midi.save('Frank.mid')
+# Enregistrer le fichier MIDI
+sortie_midi.save('Brother.mid')
